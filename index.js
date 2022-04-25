90fa3ba09f76895d203689ce9b29f176327503cb
iinaamasum <iinaamasum@gmail.com>
2022-04-25 11:26:25 +0600

    post and delete api added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 require('dotenv').config();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
 app.use(cors());
@@ -26,21 +26,44 @@ async function run() {
   try {
     await client.connect();
     const ticketCollection = client.db('travel-guru').collection('tickets');
+
     //get api to read all tickets
     // link: http://localhost:4000/tickets
     app.get('/tickets', async (req, res) => {
       const q = req.query;
-      console.log(q);
+      console.log(q); //q is representing the query which is an empty object and so returns all of the result.
 
       const cursor = ticketCollection.find(q);
 
       const result = await cursor.toArray();
 
-      res.send('hi');
+      res.send(result);
     });
 
-    //get api to read all tickets
-    // link: http://localhost:4000/tickets
+    //post api to send data to server
+    // link: http://localhost:4000/ticket
+
+    app.post('/ticket', async (req, res) => {
+      const data = req.body;
+      console.log(data);
+
+      //the data is an json format send by api from client
+
+      const result = await ticketCollection.insertOne(data);
+      console.log('sending title to server');
+      res.send(data);
+    });
+
+    app.delete('/ticket/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      console.log(id);
+
+      const result = await ticketCollection.deleteOne(filter);
+      res.send(result);
+    });
+
+    console.log('db running');
   } finally {
   }
 }
